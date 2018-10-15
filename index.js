6a6afbf744d90a3dd47cdf42919185ed1125131d
Timothy Hoang <timh1203@gmail.com>
2018-10-15 13:41:59 -0400

    Add login route

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.get('/', (req, res) => {
 
 server.post('/register', (req,res) => {
   const credentials = req.body;
-  
+
   const hash = bcrypt.hashSync(credentials.password, 14);
   credentials.password = hash;
 
@@ -25,6 +25,19 @@ server.post('/register', (req,res) => {
       res.status(201).json({ newUserId: id });
     })
     .catch(err => res.status(500).json(err));
+})
+
+server.post('/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) return res.status(200).json({ Message: `Welcome ${user.username}!` });
+      return res.status(401).json({ message: 'You shall not pass!' });
+    })
+    .catch(err => res.status(500).json({ err }));
 })
 
 // protect this route, only authenticated users should see it
