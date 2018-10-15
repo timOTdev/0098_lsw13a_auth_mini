3b26cc7b65016b877066aabceba2f3a0b18571b6
Timothy Hoang <timh1203@gmail.com>
2018-10-15 13:22:41 -0400

    Add core files and register route

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-
+const bcrypt = require('bcryptjs');
 const db = require('./database/dbConfig.js');
-
+const port = 9000
 const server = express();
 
 server.use(express.json());
@@ -12,14 +12,27 @@ server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
 
-// protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
+server.post('/register', (req,res) => {
+  const credentials = req.body;
+  
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
+
   db('users')
-    .select('id', 'username')
-    .then(users => {
-      res.json(users);
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ newUserId: id });
     })
+    .catch(err => res.status(500).json(err));
+})
+
+// protect this route, only authenticated users should see it
+server.get('/users', (req, res) => {
+  db('users')
+    .select('id', 'username', 'password')
+    .then(users => res.json(users))
     .catch(err => res.send(err));
 });
 
-server.listen(3300, () => console.log('\nrunning on port 3300\n'));
+server.listen(port, () => console.log(`\nrunning on ${port}\n`));
